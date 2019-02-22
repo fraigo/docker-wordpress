caa7511b591627d18b7920ef69fc4b9c2fdcba10
Francisco <franciscoigor@gmail.com>
2019-02-22 00:13:35 -0800

    updated theme card images

--- themes/demo/functions.php
+++ themes/demo/functions.php
@@ -39,6 +39,9 @@ if ( ! function_exists( 'demo_setup' ) ) :
 		*/
 		add_theme_support( 'title-tag' );
 
+		add_theme_support( 'post-thumbnails' );
+		set_post_thumbnail_size( 1200, 9999 );
+
 		// This theme uses wp_nav_menu() in two locations.
 		register_nav_menus( array(
 			'primary' => __( 'Primary Menu',      'demo' ),
--- themes/demo/header.php
+++ themes/demo/header.php
@@ -167,6 +167,7 @@ $themepath=esc_url(home_url() . "/wp-content/themes/" . basename(dirname(__FILE_
 	 	<?php foreach($header_posts as $p){ ?>
 			<div class="card <?php echo $col_class ?> text-center">
 				<div class="card-content">
+					<?php echo get_the_post_thumbnail($p->ID,[100,100],["class"=>"rounded"]) ?>
 					<h2 class="card-title"><?php echo $p->post_title ?></h2>
 					<p class="card-text"><?php echo $p->post_content ?></p>
 				</div>
